5aafc563f0c80ad59693550503894019d9223ce7
Andrew Rosen <rosen@cs.gsu.edu>
2015-04-08 13:57:29 -0400

    Changed hello.c on github

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-	printf("hello, world!\n");
+	//printf("hello, world!\n");  Let's do something more interesting
+	int i;
+	for(i=0; i<3; i++){
+		printf("%d hello world! \n", i);
+	}
 }
 
